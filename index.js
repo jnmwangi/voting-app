edfeef460e0050db4de99d1bf6b4fea96dcb3bc3
Julius Njuguna <julius@techlait.com>
2022-08-12 12:19:12 +0300

    Add javascript to your html page

---
+++ index.js
@@ -0,0 +1,53 @@
+
+/** 7 data types in javascript
+ * number
+ * undefined
+ * null
+ * string
+ * object
+ * boolean
+ * symbols
+ */
+
+/* const candidateNames = ["Chris", "Julius", "Sialo"]; // array of strings
+const candidateVotes = [23, 76, 133]; // array of number */
+
+const candidates = [ // array of object
+    {
+        name: "Chris", // Key Value Pair
+        votes: 0,
+        partyName: "Javascript"
+    },
+
+    {
+        name: "Julius", // Key Value Pair
+        votes: 0,
+        partyName: "GIT"
+    },
+
+    {
+        name: "Sialo", // Key Value Pair
+        votes: 0,
+        partyName: "Ruby"
+    }
+];
+
+console.log( candidates );
+
+/* const candidate1 = {
+        name: "Chris", // Key Value Pair
+        votes: 0,
+        partyName: "Javascript"
+    }
+
+const candidate2 = {
+        name: "Julius", // Key Value Pair
+        votes: 0,
+        partyName: "GIT"
+    }
+
+const candidate3 = {
+    name: "Sialo", // Key Value Pair
+    votes: 0,
+    partyName: "Ruby"
+} */
